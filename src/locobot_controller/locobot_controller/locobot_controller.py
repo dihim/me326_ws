75bc44b6447db13e5a1d1395874e48df1905ccc0
Ian Chang <ianchang@stanford.edu>
2024-02-21 19:53:04 -0800

    fixing typo

--- src/locobot_controller/locobot_controller/locobot_controller.py
+++ src/locobot_controller/locobot_controller/locobot_controller.py
@@ -14,7 +14,7 @@ class LocobotController(Node):
         self.image_subscriber = self.create_subscription(Image, '/locobot/camera_frame_sensor/image_raw', self.image_callback, 10)
         self.vel_publisher = self.create_publisher(Twist, '/locobot/diff_drive_controller/cmd_vel_unstamped', 10)
         self.target_pose_publisher = self.create_publisher(PointStamped, '/locobot_controller/block_pose', 10)
-        self.image_subscriber = self.create_subscription(PointStamped '/locobot_controller/block_pose', self.navigate_to_block, 10)
+        self.image_subscriber = self.create_subscription(PointStamped, '/locobot_controller/block_pose', self.navigate_to_block, 10)
         self.bridge = CvBridge()
         self.target_pose = None
 
