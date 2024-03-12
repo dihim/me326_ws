ee6171c065bcc5644b5166f62cdb5c62c08f2fbd
Ian Chang <ianchang@stanford.edu>
2024-03-12 14:58:44 -0700

    enabling callback subscribe

--- src/locobot_controller/locobot_controller/locobot_perception.py
+++ src/locobot_controller/locobot_controller/locobot_perception.py
@@ -3,47 +3,63 @@ from rclpy.node import Node
 from la_msgs.srv import Ptps
 from std_msgs.msg import String
 from geometry_msgs.msg import PointStamped
-
+from rclpy.action import ActionClient
+import sys
 
 class PerceptionClient(Node): 
 
     def __init__(self): 
         super().__init__('perception_client')
-        self.cli = self.create_client(Ptps, 'pix_to_point_cpp') 
-        while not self.cli.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('service not available, waiting again')
+        self.get_logger().info("Starting Node")
+        self.client = self.create_client(Ptps, 'pix_to_point_cpp')
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service not available, waiting again...') 
         self.req = Ptps.Request()
-        self.subscription = self.create_subscription(String, '/desired_color', self.listener_callback, 10)
-        self.publisher = self.create_publisher(PointStamped, '/block_location', 10)
+        self.subscription = self.create_subscription(String, 'desired_color', self.listener_callback, 10) 
+        self.publisher = self.create_publisher(String, 'topic', 10) 
+        self.get_logger().info("Service found, setting up")
+        self.desired_frame = 'locobot/odom'
+        self.client_futures = []
+        self.point = None
 
-    def listener_callback(self, msg): 
-        point = self.send_request('locobot/base_link', msg.data) 
-        self.publisher.publish(point)
+    def listener_callback(self, msg):
+        self.desired_color = msg.data
+        print("Searching for color : " + msg.data)
+        self.req.desired_frame = self.desired_frame
+        self.client_futures.append(self.client.call_async(self.req))
 
-    def send_request(self, desired_frame, color): 
-        self.req.desired_frame = desired_frame
-        self.future = self.cli.call_async(self.req)
-        rclpy.spin_until_future_client(self, self.future)
-        result = self.future.result()
-        if color == 'red':
-            if result.red_present:
-                return result.red_points[0]
-        elif color == 'blue': 
-            if result.blue_present: 
-                return result.blue_points[0]
-        elif color == 'yellow':
-            if result.yellow_present:
-                return result.yellow_points[0]
-        elif color == 'green': 
-            if result.green_present:
-                return result.green_points[0]
-
-
+    def spin(self):
+        self.get_logger().info("Spinning is a fun trick")
+        while rclpy.ok():
+            rclpy.spin_once(self)
+            incomplete_futures = []
+            for f in self.client_futures:
+                if f.done():
+                    result = f.result()
+                    self.get_logger().info('Found Block') 
+                    if self.desired_color == 'red':
+                        if result.red_present and (len(result.red_points) != 0):
+                            self.point = result.red_points[0]
+                    elif self.desired_color == 'blue' and (len(result.blue_points) != 0): 
+                        if result.blue_present: 
+                            self.point = result.blue_points[0]
+                    elif self.desired_color == 'yellow' and (len(result.yellow_points) != 0):
+                        if result.yellow_present:
+                            self.point = result.yellow_points[0]
+                    elif self.desired_color == 'green'and (len(result.green_points) != 0): 
+                        if result.green_present:
+                            self.point = result.green_points[0]
+                    if self.point != None:
+                        self.get_logger().info("Publishing block location") 
+                        self.publisher.publish(self.point)
+                else:
+                    incomplete_futures.append(f) 
+            self.client_futures = incomplete_futures 
 
 def main(args=None):
     rclpy.init(args=args)
     perception_client = PerceptionClient()
-    rclpy.spin(perception_client)
+    perception_client.spin()
     perception_client.destroy_node()
     rclpy.shutdown()
 
