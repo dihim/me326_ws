bfe6edf15c96073f3c227c37726acd4f3bd3ea42
Ian Chang <ianchang@stanford.edu>
2024-03-13 15:05:12 -0700

    adding movement and kennedy code

---
+++ src/locobot_autonomy/build/locobot_autonomy/locobot_autonomy__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ian339/collaborative_robotics_course/locobot_autonomy/build/locobot_autonomy/locobot_autonomy__py
+# Build directory: /home/ian339/collaborative_robotics_course/locobot_autonomy/build/locobot_autonomy/locobot_autonomy__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
