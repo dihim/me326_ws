9fab0db69d0fb78bccf0803a7633e63fc5e01fa4
Ian Chang <ianchang@stanford.edu>
2024-03-13 18:40:34 -0700

    updated strategy

--- src/locobot_controller/locobot_controller/locobot_strategy.py
+++ src/locobot_controller/locobot_controller/locobot_strategy.py
@@ -12,16 +12,18 @@ class StrategyNode(Node):
     def __init__(self): 
         super().__init__('strategy_node')
         self.color_publisher = self.create_publisher(String, 'desired_color', 10)
-        self.picked_up_block_sub = self.create_subscription(Boolean, 'pick_block_response', self.picked_up_block_callback, 10) 
-        self.dropped_off_block_sub = self.create_subscription(Boolean, 'drop_block_response', self.dropped_up_block_callback, 10) 
+        self.picked_up_block_sub = self.create_subscription(Bool, 'pick_block_response', self.picked_up_block_callback, 10) 
+        self.dropped_off_block_sub = self.create_subscription(Bool, 'drop_block_response', self.dropped_off_block_callback, 10) 
         self.block_pose_sub = self.create_subscription(PointStamped, 'block_location', self.block_location_callback, 10) 
         self.target_pose_pub = self.create_publisher(PointStamped, 'target_pose', 10)
-        self.block_arm_pose_pub = self.create_publisher(PoseStamped, 'pick_block') 
-        self.drop_block_pub = self.create_publisher(PoseStamped, 'drop_block') 
+        self.block_arm_pose_pub = self.create_publisher(PoseStamped, 'pick_block', 10) 
+        self.drop_block_pub = self.create_publisher(PoseStamped, 'drop_block', 10) 
         self.reach_block_sub = self.create_subscription(PoseStamped, 'reached_position', self.reached_position_callback, 10) 
-        self.drop_off_point = (-1.0, -1.0, 0) 
-        self.header = Header(stamp = rospy.Time.now(), frame_id='odom') 
-        self.drop_off_point_stamped = PointStamped(header = self.header, point = self.drop_off_point)
+        self.drop_off_point = Point()
+        self.drop_off_point.x = -1.0
+        self.drop_off_point.y = -1.0
+        header = Header()
+        self.drop_off_point_stamped = PointStamped(header = header, point = self.drop_off_point)
         self.target_color = 'green'
         self.reached_block = False
         self.has_block = False
@@ -42,9 +44,9 @@ class StrategyNode(Node):
         
 
     def block_location_callback(self, msg):
-        if self.reached_block = False: 
+        if self.reached_block == False: 
             self.target_pose_pub.publish(msg) 
-        elif self.reached_block = True: 
+        elif self.reached_block == True: 
             block_pose = PoseStamped()
             block_pose.Pose.Point = msg.Point
             block_pose.Pose.Orientation = self.robot_pose.Orientation
@@ -52,12 +54,12 @@ class StrategyNode(Node):
             self.reached_block = False
 
     def picked_up_block_callback(self, msg):
-        if (msg.data = True): 
+        if (msg.data): 
             self.has_block = True
             self.target_pose_pub.publish(self.drop_off_point_stamped) 
 
     def dropped_off_block_callback(self, msg): 
-        if (msg.data = True):
+        if (msg.data):
             if self.target_color == 'green': 
                 self.target_color = 'red'
                 self.get_block('red') 
